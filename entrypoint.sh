1f6d6a8a42b8a39fd567ee7e89211ca19327f535
pqab <pqab.annie@gmail.com>
2020-03-16 18:22:58 +0900

    Add jq to get milestone name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -u
 
+# Check name is 'milestone'
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+    echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
+    exit 0
+fi
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+# Check milestone was closed
+if [ $event_type != 'close' ]; then
+    echo "::debug::The event type was '$event_type'"
+    exit 0
+fi
+
+# Get milestone name
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
 echo "::set-output name=release-url::http://example.com"
 
 exit 0
